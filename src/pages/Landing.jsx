f5fc0264c7cb53eb5bdef679f3d8270bb7b16e06
abdullah <abdullamsuleman@gmail.com>
2026-08-12 20:53:07 +0300

    landing page update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router"
+
 const Landing = () => {
     return (
         <section className="card">
             <h1>Welcome!</h1>
             <p>Sign up or sign in to see your dashboard.</p>
+
+            <div className="actions">
+                <Link to="/sign-up">
+                    <button>Sign Up</button>
+                </Link>
+                <Link to="/sign-in">
+                    <button>Sign In</button>
+                </Link>
+            </div>
 
         </section>
     )
