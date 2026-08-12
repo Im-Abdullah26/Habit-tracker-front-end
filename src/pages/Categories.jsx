119b4fa244dac8e78c3b58f08f37697f8de45861
abdullah <abdullamsuleman@gmail.com>
2026-08-12 20:10:48 +0300

    Categories.jsx page

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router"
+
+const Categories = (props) => {
+  const { categories, handleDeleteCategory } = props
+
+  return (
+<section>
+ <header>
+    <h1>Your Categories</h1>
+ </header>
+
+</section>
+  )
+}
+
+export default Categories
