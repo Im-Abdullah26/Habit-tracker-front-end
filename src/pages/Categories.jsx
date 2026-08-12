5e18e149ef8ada8d29f0b57e23d96a3527ed06af
abdullah <abdullamsuleman@gmail.com>
2026-08-12 20:13:35 +0300

    empty state message

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -8,7 +8,12 @@ const Categories = (props) => {
  <header>
     <h1>Your Categories</h1>
  </header>
-
+ {categories.length === 0 && (
+  <div className="card">
+    <p>You don't have any categories yet.</p>
+    <Link className="Edit" to="/categories/new">Create your first category</Link>
+</div>
+)}
 </section>
   )
 }
